fc6e8fec39112c7c2ad7ef672236ccc89c0a3a3e
tundeorekoya <tundeorekoya1998@gmail.com>
2024-03-27 08:12:53 -0700

    update

--- src/components/Cryptocurrency.jsx
+++ src/components/Cryptocurrency.jsx
@@ -1,29 +1,57 @@
-import { Card, Row, Col, input } from "antd"
+import { Card, Row, Col, input, FloatButton } from "antd"
 import { Link } from "react-router-dom"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useGetCryptosQuery } from "../services/cryptoApi"
 import millify from "millify"
+
 const Cryptocurrency = () => {
-  const { data: cryptoList, isFetch } = useGetCryptosQuery()
-  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins)
+  const { data: cryptoList, isFetching } = useGetCryptosQuery()
+  const [cryptos, setCryptos] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  useEffect(() => {
+
+    const FetchCoin = cryptoList?.data?.coins.filter((coins) => coins.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    setCryptos(FetchCoin)
+
+  }, [searchTerm, cryptoList])
+
+
+  if (isFetching) {
+    return 'Loading....'
+
+  }
+
   return (
-    <Row gutter={[32, 32]} className="crypto-card-container" >
+    <>
+      <div className="search-crypto" >
+        <input className="search-input" placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)} />
+
+      </div>
+    <Row gutter={[32, 32]} className="crypto-card-container" style={{ marginTop: "0.5rem", marginBottom: "0.5rem" }}>
       {
-        cryptos.map((currency) => (
+        cryptos?.map((currency) => (
           <Col className="crypto-card" key={currency.id} xs={24} sm={12} lg={6}>
             <Link to={`/crypto/${currency.id}`}>
               <Card title={`${currency.rank} ${currency.name}`} extra={<img className="crypto-image" src={`${currency.iconUrl}`} />} hoverable>
                 <p> Price: {millify(currency.price)}</p>
                 <p> Market Cap: {millify(currency.marketCap)}</p>
-                <p> Daily Change: {millify(currency.change)}</p>
+                <p> Daily Change: {millify(currency.change)}%</p>
 
               </Card>
             </Link>
           </Col>
         ))
       }
+      <FloatButton.BackTop />
     </Row>
+    </>
+
   )
 }
 
 export default Cryptocurrency
+
+
+
+
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,6 @@
 import millify from "millify"
 import { Link } from "react-router-dom"
-import { Typography, Statistic, Row, Col } from "antd"
+import { Typography, Statistic, Row, Col, Card } from "antd"
 import { useGetCryptosQuery } from "../services/cryptoApi";
 import { News, Cryptocurrency } from "../components"
 
@@ -13,26 +13,29 @@ const Homepage = () => {
     return 'Loading....'
   }
   console.log(data)
+
   return (
     <>
+      
       <Title level={2} className="heading">Global Crypto Statistic</Title>
-      <Row>
-        <Col span={12} ><Statistic value={globalStat.totalCoins} title="Total Cryptocurrencies" /></Col>
-        <Col span={12} ><Statistic value={millify(globalStat.totalExchanges)} title="Total Exchanges" /></Col>
-        <Col span={12} ><Statistic value={millify(globalStat.totalMarketCap)} title="Total Market Cap" /></Col>
-        <Col span={12} ><Statistic value={millify(globalStat.total24hVolume)} title="Total 24h" /></Col>
-        <Col span={12} ><Statistic value={millify(globalStat.totalMarkets)} title="Total Market" /></Col>
+      <Row gutter={[16, 16]}>
+        <Col span={6}  > <Card hoverable ><Statistic value={globalStat.totalCoins} title="Total Cryptocurrencies" /></Card>
+        </Col>
+        <Col span={6} ><Card hoverable><Statistic value={millify(globalStat.totalExchanges)} title="Total Exchanges" /></Card></Col>
+        <Col span={6} ><Card hoverable><Statistic value={millify(globalStat.totalMarketCap)} title="Total Market Cap" /></Card></Col>
+        <Col span={6} ><Card hoverable><Statistic value={millify(globalStat.total24hVolume)} title="Total 24h" /></Card></Col>
+        <Col span={6} ><Card hoverable><Statistic value={millify(globalStat.totalMarkets)} title="Total Market" /></Card></Col>
       </Row>
       <div className="home-heading-container">
         <Title level={2} className="home-title">Top 10 CryptoCurrency in The World</Title>
         <Title level={4} className="show-more" style={{ marginRight: "1rem" }}><Link to="./Cryptocurrency">Show more</Link></Title>
       </div>
-      <Cryptocurrency simplified/>
+      <Cryptocurrency />
       <div className="home-heading-container">
         <Title level={2} className="home-title">Latest Crypto News</Title>
         <Title level={4} className="show-more" style={{ marginRight: "1rem" }}><Link to="./News">Show More</Link></Title>
       </div>
-      <News/>
+      <News />
     </>
   )
 }
